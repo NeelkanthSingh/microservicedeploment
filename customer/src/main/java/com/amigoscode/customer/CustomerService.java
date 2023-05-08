fd8c87a7437b2af5b907a956573710c8b3f61f26
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-05-09 02:22:05 +0530

    customer microservice is ready

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,16 @@
+package com.amigoscode.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .email(request.email())
+                .lastName(request.lastName())
+                .build();
+
+        customerRepository.save(customer);
+    }
+}
